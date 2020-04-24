f1ff58f689980757ddc4ecbbd9d5167087c86bc2
Your Name <you@example.com>
2020-04-24 12:18:40 -0400

    first commit, created desired compoents, i.e form and orders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { Route, Link, useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import Form from './components/Form'
+import Orders from './components/Orders'
+import logo from './images/pizza.jpg';
 
 const App = () => {
+  // let history = useHistory();
+  // let routeHome = () => (
+  //   history.push('/')
+  // )
   return (
     <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <Link to='/'>Homepage</Link>
+      </nav>
+    </div>
+    <Route exact path = '/'>
+      <div>
+        <h1>Build your own pizza!</h1>
+        {/* <a className='home-button' onClick={routeHome}><img src={hero} className='hero-img' alt='pizza'/></a> */}
+        <Link to='/form' className='img-to-form'><img src={logo} className="hero" alt="logo" /></Link>
+        <h1>Lets get started!</h1>
+      </div>
+    </Route>
+    
+    <Route path='/form'>
+      <Form/>
+    </Route>
+
+    <Route path = '/orders'>
+      <Orders/>
+    </Route>
     </>
   );
 };
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Form(props){
+    return (
+        <div>Inside Form</div>
+    )
+}
+
+export default Form;
